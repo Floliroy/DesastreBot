0a7080f701e99440d92a9571abf0c33a7ed0c825
Flo Leeroy <florian.leeroy@hotmail.com>
2020-05-07 15:44:23 +0200

    id updated + welcome mess updated

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ bot.on('ready', () => {
     console.log(`RUNNING: ${bot.user.tag}`)
     bot.user.setActivity("DesastreShow", {type: "WATCHING"})
     //Fetch sur le message ajoutant un role par reaction
-    bot.channels.cache.get(channelsId.general).messages.fetch(messagesId.roles)
+    bot.channels.cache.get(channelsId.regles).messages.fetch(messagesId.roles)
 })
 
 const nodeColors ={
@@ -24,33 +24,36 @@ const nodeColors ={
     white: "\x1b[37m",
     reset: "\x1b[0m",
 }
-//test 1 (696392072835498047)
-//test 2 (696392106272358480)
-//test 3 (696392133145395271)
+
 const rolesId = {
-    //TODO : Changer pour les bons IDs de role
-    admin: "696392106272358480",
-    conseiller: "696392133145395271",
-    pc: "696392072835498047",
-    ps4: "696392106272358480",
-    xbox: "696392133145395271",
+    admin: "385850055426572298",
+    modo: "426426746171293698",
+    conseiller: "671356231415365642",
+    helpeur: "706869753498697730",
+    sub: "380413691339997194",
+
+    pc: "428584658003820545",
+    ps4: "428584774848872458",
+    xbox: "428584720125919243",
 }
-//test role (706565906104320110)
+
 const messagesId = {
-    roles: "706565906104320110",
+    roles: "", //TODO
 }
-//test general (696367866752139385)
+
 const channelsId = {
-    general: "696367866752139385",
+    regles: "489125908666515456",
+    giveaway : "", //TODO
 }
-//test server (696367866319994961)
-const serversId = {
-    desastre: "696367866319994961",
+
+const usersID = {
+    desastre: "146003404840239104",
 }
+
 //Vérifie que la personne passé en paramètre ai un des rôles nécessaire
 function isAuthorised(member){
     let roles = member.roles.cache
-    return roles.has(rolesId.admin) || roles.has(rolesId.conseiller)
+    return roles.has(rolesId.admin) || roles.has(rolesId.modo) || roles.has(rolesId.conseiller) || roles.has(rolesId.helpeur)  || roles.has(rolesId.sub)
 }
 //Permet d'envoyer un message privé à la personne passé en paramètre
 function sendPrivateMessage(member, message){
@@ -62,9 +65,9 @@ function sendPrivateMessage(member, message){
 //Listener quand un message est envoyé sur le serveur
 bot.on('message', function (message) {
     if(message.author === bot.user || message.channel instanceof Discord.DMChannel) return
-    if(message.content.startsWith("!rand ")){
+    if(message.content.startsWith("!rand ") && message.author.id == usersID.desastre){
         const args = message.content.split(" ")
-        bot.channels.cache.get(channelsId.general).messages.fetch(args[1]).then(msg => {
+        bot.channels.cache.get(channelsId.giveaway).messages.fetch(args[1]).then(msg => {
             const reaction = msg.reactions.cache.get('✅')
 
             reaction.users.fetch().then(users => {
@@ -172,7 +175,14 @@ bot.on("messageReactionRemove", (reaction, user) => {
 bot.on('guildMemberAdd', member => {
     //On lui envoit un message de bienvenue
     let messageEmbed = new Discord.MessageEmbed()
-    .setTitle("Bienvenue sur le super Discord de DesastreShow !")
+    .setTitle("Bienvenue sur le Discord de Desastre_Show")
+    .setDescription(`Prenez 2 minutes pour lire les <#${channelsId.regles}> et indiquer votre plateforme pour avoir accès à la totalité du Discord.\n` +
+                    "Vous trouverez des canaux écrits pour discuter, partager et demander des conseils sur TESO.\n" +
+                    "Des canaux vocaux sont également a votre disposition pour jouer avec vos amis.\n\n" +
+                    "Desastre est en Live tous les jours de 20h à 23h à cette adresse : https://www.twitch.tv/desastre_show\n" +
+                    "Vous trouverez des Builds à jours, des Guides et Tutoriels sur https://desastreshow.com\n" +
+                    "Pour ceux qui préfère les vidéos, vous trouverez des Guides du Débutant, des infos et des news sur YouTube.")
+    .setThumbnail("https://ibb.co/R9k9BkP")
 
     console.log(`LOG: '${nodeColors.green}${member.user.tag}${nodeColors.reset}' got his welcome message`)
     sendPrivateMessage(member, messageEmbed)
