347e6cf5c32fb961cb9fab166d7029346a463368
Flo Leeroy <florian.leeroy@hotmail.com>
2022-08-02 14:46:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,10 @@ bot.on('ready', async function(){
         name: `Membres : ${bot.guilds.cache.get(guildsId.desastre).memberCount}`
     })
     const msg = await bot.channels.cache.get(channelsId.regles).messages.fetch(messagesId.roles)
+    const icone = client.emojis.cache.find(emoji => emoji.name === "desastreEcostream");
     const embed = new Discord.MessageEmbed()
         .setTitle("VOS RÔLES")
-        .setDescription("Merci d'indiquer votre plateforme en réagissant à ce message :\n**PC** :orange_circle: │ **PS4** :blue_circle: │**XBOX** :green_circle:\n\nVous pouvez récupérer le rôle <@&1003996572914286612> en cliquant sur <:desastreEcostream:978383826776633404>")
+        .setDescription(`Merci d'indiquer votre plateforme en réagissant à ce message :\n**PC** :orange_circle: │ **PS4** :blue_circle: │**XBOX** :green_circle:\n\nVous pouvez récupérer le rôle <@&1003996572914286612> en cliquant sur ${icone}`)
         .setThumbnail("https://download.seaicons.com/icons/paomedia/small-n-flat/1024/gamepad-icon.png")
     msg.edit(embed)
     //On se connecte au GDoc
